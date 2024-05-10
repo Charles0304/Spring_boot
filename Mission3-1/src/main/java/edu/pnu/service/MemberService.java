0df87f2a3c9fa5f6cad443d025a7bdea23db3793
Charles0304 <cotncjf2@gmail.com>
2024-05-10 15:11:34 +0900

    0510

---
+++ Mission3-1/src/main/java/edu/pnu/service/MemberService.java
@@ -0,0 +1,39 @@
+package edu.pnu.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import edu.pnu.dao.MemberDao;
+import edu.pnu.domain.MemberVO;
+
+public class MemberService {
+	MemberDao dao;
+	public MemberService() {
+	
+	}
+	public MemberService(MemberDao dao) {
+		this.dao = dao;
+	}
+	public void setMemberDao(MemberDao dao) {
+		this.dao = dao;
+	}
+	public List<MemberVO> getMembers() throws SQLException{
+		return dao.getMembers();
+	}
+	
+	public MemberVO getMemberById(Integer id) throws SQLException {
+		return dao.getMemberById(id);
+	}
+	
+	public MemberVO insertMember(MemberVO m) throws SQLException {
+		return dao.InsertMember(m);
+	}
+	
+	public int updateMember(MemberVO m) throws SQLException {
+		return dao.UpdateMember(m);
+	}
+	
+	public int deleteMember(Integer id) throws SQLException {
+		return dao.DeleteMember(id);
+	}
+}
